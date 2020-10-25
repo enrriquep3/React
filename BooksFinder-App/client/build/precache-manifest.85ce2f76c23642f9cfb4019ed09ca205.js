5ae2ba52a774f21c1249510ecb8949618c6384a2
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-10-25 08:53:14 -0400

    finalapp

---
+++ BooksFinder-App/client/build/precache-manifest.85ce2f76c23642f9cfb4019ed09ca205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e773aded932af3a7776",
+    "url": "/static/css/main.eb0c5c60.chunk.css"
+  },
+  {
+    "revision": "5e773aded932af3a7776",
+    "url": "/static/js/main.1e277416.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad682cc1190f558d878f",
+    "url": "/static/js/2.858a9108.chunk.js"
+  },
+  {
+    "revision": "57c42f62230cf6e62502ee90d6e52d9b",
+    "url": "/index.html"
+  }
+];
